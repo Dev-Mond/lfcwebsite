2df8f63da2c8330c4ee74536a185d9553a3277d7
Dev-Mond <zhenbeen4@gmail.com>
2020-08-17 21:41:11 +0800

    first commit

---
+++ administrator/logs/error.php
@@ -0,0 +1,9 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2020-08-17 07:34:21 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2020-08-17T07:34:21+00:00	INFO ::1	joomlafailure	Empty password not allowed.
+2020-08-17T10:53:48+00:00	INFO ::1	joomlafailure	Empty password not allowed.
+2020-08-17T11:54:59+00:00	INFO ::1	joomlafailure	Empty password not allowed.
---
+++ administrator/logs/joomla_update.php
@@ -0,0 +1,14 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2020-08-17 09:21:28 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2020-08-17T09:21:28+00:00	INFO ::1	update	Update started by user Super User (735). Old version is 3.9.20.
+2020-08-17T09:21:30+00:00	INFO ::1	update	Downloading update file from https://s3-us-west-2.amazonaws.com/joomla-official-downloads/joomladownloads/joomla3/Joomla_3.9.20-Stable-Update_Package.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIZ6S3Q3YQHG57ZRA%2F20200817%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20200817T092123Z&X-Amz-Expires=60&X-Amz-SignedHeaders=host&X-Amz-Signature=1096ae559d6697415a1560d00608f34cfa2f89a41e24fff161449be6b5ec3f9c.
+2020-08-17T09:21:34+00:00	INFO ::1	update	File Joomla_3.9.20-Stable-Update_Package.zip downloaded.
+2020-08-17T09:21:35+00:00	INFO ::1	update	Starting installation of new version.
+2020-08-17T09:24:50+00:00	INFO ::1	update	Finalising installation.
+2020-08-17T09:25:06+00:00	INFO ::1	update	Deleting removed files and folders.
+2020-08-17T09:25:09+00:00	INFO ::1	update	Cleaning up after installation.
+2020-08-17T09:25:09+00:00	INFO ::1	update	Update to version 3.9.20 is complete.
